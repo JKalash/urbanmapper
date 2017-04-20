f941e931aeb61271e73f6428aff9963657e78d88
Joseph <me@jkalash.com>
2017-04-20 13:26:31 +0300

    Initial Commit

---
+++ UrbanMapper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UrbanMapper
+//
+//  Created by Joseph Kalash on 4/20/17.
+//  Copyright © 2017 Joseph Kalash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
